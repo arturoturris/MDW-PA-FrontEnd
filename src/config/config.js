58c45c7d9c0b59a7a84f712684415b32face77ef
shelkanife <81986306+shelkanife@users.noreply.github.com>
2021-04-14 17:11:55 -0500

    Delete src directory

--- src/config/config.js
+++
@@ -1,9 +0,0 @@
-const APP_PORT = process.env.port || 3000
-const APP_HOST = `http://localhost/${APP_PORT}`
-const API_URL = `http://localhost:3300/api/v1/`
-
-module.exports = {
-    APP_PORT,
-    APP_HOST,
-    API_URL
-}

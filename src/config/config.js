deae6f9e98fbb89e543b2015bcfa821c2285f058
Arturo Tenorio <arturo-turris@hotmail.com>
2021-09-08 19:07:15 -0500

    V1.0.1 - Adding dotenv file and preparing for a deploy in heroku

--- src/config/config.js
+++ src/config/config.js
@@ -1,16 +1,12 @@
-const ENV = process.argv[2]
-const HOST = ENV == 'production' ?
-    'http://3.142.69.247' :
-    'http://localhost'
-const APP_PORT = process.env.port || 3000
-const APP_HOST = `${HOST}:${APP_PORT}`
-const API_URL = ENV == 'production' ?
-    'http://3.142.69.247:3300/api/v1/' :
-    'http://localhost:3300/api/v1/'
+const ENV = process.env.NODE_ENV;
+const HOST = process.env.HOST;
+const APP_PORT = process.env.port || 3000;
+const APP_HOST = `${HOST}:${APP_PORT}`;
+const API_URL = process.env.API_URL;
 
 module.exports = {
-    ENV,
-    APP_PORT,
-    APP_HOST,
-    API_URL,
-}
+  ENV,
+  APP_PORT,
+  APP_HOST,
+  API_URL,
+};
